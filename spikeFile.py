a760a82dabdb1f3fe73e44b41a7c38c6c694fef1
Anindya Ghosh <anindya128@yahoo.com>
2022-07-19 11:30:44 +0100

    Added temp cluster before lunch 19/07

--- spikeFile.py
+++ spikeFile.py
@@ -1,11 +1,18 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 
 currDir = os.path.dirname(os.path.abspath(__file__))
 
 foldername = 'Cori_2016-12-14'
-filename = 'clusters.templateWaveforms.npy'
+filename = ['spikes.times.npy', 'spikes.clusters.npy']
 
-file = os.path.join(currDir, 'spikeAndBehavioralData\\allData', foldername, filename)
+data = np.zeros((10017476, 2))
 
-data = np.load(file)
+for i in np.arange(len(filename)):
+    file = os.path.join(currDir, 'spikeAndBehavioralData\\allData', foldername, filename[i])
+    data[:,i] = np.load(file).flatten()
+
+# [times, spikes]
+
+plt.scatter(data[:100, 0], data[:100, 1])
